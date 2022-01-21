7619795900e61f7395315ffa40770957b12a54e6
daria <bakhtyzina.dar@gmail.com>
2022-01-21 22:59:01 +0200

    HW2 Ex3

--- src/Task3.java
+++ src/Task3.java
@@ -1,16 +1,17 @@
 public class Task3 {
-    public static void main(String[] args){
-         //Ex3
+    public static void main(String[] args) {
+        //Ex3
         float a = -34;
-        float b = -65;
+        float b = -46;
         float c = 27;
         System.out.println(a + " and " + b + " and " + c);
         minAbsolute(a, b, c);
     }
+
     private static void minAbsolute(float a, float b, float c) {
-        float n1 = Math.abs(a);
-        float n2 = Math.abs(b);
-        float n3 = Math.abs(c);
+        float n1 = a >= 0 ? a : -a;
+        float n2 = b >= 0 ? b : -b;
+        float n3 = c >= 0 ? c : -c;
         System.out.println("Min absolute number: " + (Math.min(n1, n2) < n3 ? Math.min(n1, n2) : n3));
     }
 }
