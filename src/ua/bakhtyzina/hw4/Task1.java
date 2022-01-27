e7112f07b9189557ebaf8df69a53a70f1b11217a
daria <bakhtyzina.dar@gmail.com>
2022-01-27 23:37:58 +0200

    HW3 Task2

---
+++ src/ua/bakhtyzina/hw4/Task1.java
@@ -0,0 +1,33 @@
+package ua.bakhtyzina.hw4;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int[] numbers = generateArray();
+        System.out.println(Arrays.toString(numbers));
+        averageValue(numbers);
+    }
+
+    private static int[] generateArray() {
+        int[] array = new int[400];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (1 + Math.random() * 10);
+        }
+        return array;
+    }
+
+    private static void averageValue(int[] numbers) {
+        int[] array = Arrays.copyOf(numbers, numbers.length);
+        double ar = 0;
+        double geom = 1.0;
+        for (int i = 0; i < array.length; i++) {
+            ar += array[i];
+            geom *= array[i];
+        }
+        System.out.println("Arithmetic mean = " + ar / numbers.length);
+        System.out.println("Geometric mean = " + Math.pow(geom, 1 / numbers.length));
+    }
+
+}
