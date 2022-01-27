18171690f24d119b027d93795923f05a6c2ca803
daria <bakhtyzina.dar@gmail.com>
2022-01-28 00:43:29 +0200

    HW4 Task1

---
+++ src/ua/bakhtyzina/hw4/Task4.java
@@ -0,0 +1,30 @@
+package ua.bakhtyzina.hw4;
+
+import java.util.Arrays;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int[] numbers = generateArray();
+        int[] changeNumber = changeArrayForZero(numbers);
+        System.out.println(Arrays.toString(numbers));
+        System.out.println("New Array = " + Arrays.toString(changeNumber));
+    }
+
+    private static int[] generateArray() {
+        int[] array = new int[2000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 4000 - 2000);
+        }
+        return array;
+    }
+
+    private static int[] changeArrayForZero(int[] number) {
+        int[] array = Arrays.copyOf(number, number.length);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                array[i] = 0;
+            }
+        }
+        return array;
+    }
+}
